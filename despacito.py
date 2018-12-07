ee527240679e319b32aa81300ffffc6ba74502bf
fallen-shinigami <fallen-shinigami@users.noreply.github.com>
2018-12-07 09:06:19 -0800

    Add files via upload
    Added more entries, will refer to a list instead after completing meme list.

--- despacito.py
+++ despacito.py
@@ -1,4 +1,7 @@
+#I did a thing
+
 import webbrowser
+
 
 def despacito(alexa):
 	if alexa == "sad":
@@ -7,7 +10,22 @@ def despacito(alexa):
         	return webbrowser.open("https://www.youtube.com/watch?v=TrZk3igdICc", new=2)
         elif alexa == "beefy":
                 return webbrowser.open("https://www.youtube.com/watch?v=LYhF9ZRjVek", new=2)
-        else:
-		return print("alexa is not sad or spicy")
+       elif alexa == "cancer":
+                return webbrowser.open("https://www.youtube.com/watch?v=W3GrSMYbkBE", new=2)        
+       elif alexa == "minecraft":
+                return webbrowser.open("https://www.youtube.com/watch?v=Gl6ekgobG2k", new=2)  
+       elif alexa == "english":
+                return webbrowser.open("https://youtu.be/TVudARY3SD4?t=54", new=2)  		
+       elif alexa == "trump":
+                return webbrowser.open("https://www.youtube.com/watch?v=X3q9RdU3he0", new=2)
+       elif alexa == "roblox":
+                return webbrowser.open("https://www.youtube.com/watch?v=A6I3U4iLITQ", new=2)  					
+       elif alexa == "america":
+                return webbrowser.open("https://youtu.be/6cmg75bgkkg", new=2) 		
+       elif alexa == "a chicken wing":
+                return webbrowser.open("https://youtu.be/tLUaycJFjWA", new=2) 				
+		else:
+			return print("alexa doesn't know what this is")
 
 despacito(input("Alexa this is "))
+
